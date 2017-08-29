398464421239ccb476a15148b4d3028cd57e7efe
chandsi <guptachandsi1994@gmail.com>
2017-08-29 13:31:07 +0530

    new Design Patterns

--- TestBuilderPattern.java
+++ TestBuilderPattern.java
@@ -1,77 +1,90 @@
-
- class Computer {
-	
-
-	private String HDD;
-	private String RAM;
-
-
-	private boolean isGraphicsCardEnabled;
-	private boolean isBluetoothEnabled;
-	public String getHDD() {
-		return HDD;
-	}
-	public String getRAM() {
-		return RAM;
-	}
-
-	public boolean isGraphicsCardEnabled() {
-		return isGraphicsCardEnabled;
-	}
-
-	public boolean isBluetoothEnabled() {
-		return isBluetoothEnabled;
-	}
-	
-	private Computer(ComputerBuilder builder) {
-		this.HDD=builder.HDD;
-		this.RAM=builder.RAM;
-		this.isGraphicsCardEnabled=builder.isGraphicsCardEnabled;
-		this.isBluetoothEnabled=builder.isBluetoothEnabled;
-	}
-	
-
-	public static class ComputerBuilder{
-
-
-		private String HDD;
-		private String RAM;
-
-		private boolean isGraphicsCardEnabled;
-		private boolean isBluetoothEnabled;
-		
-		public ComputerBuilder(String hdd, String ram){
-			this.HDD=hdd;
-			this.RAM=ram;
-		}
-
-		public ComputerBuilder setGraphicsCardEnabled(boolean isGraphicsCardEnabled) {
-			this.isGraphicsCardEnabled = isGraphicsCardEnabled;
-			return this;
-		}
-
-		public ComputerBuilder setBluetoothEnabled(boolean isBluetoothEnabled) {
-			this.isBluetoothEnabled = isBluetoothEnabled;
-			return this;
-		}
-		
-		public Computer build(){
-			return new Computer(this);
-		}
-
-	}
-
-}
-
-public class TestBuilderPattern {
-
-	public static void main(String[] args) {
 			
-		Computer comp = new Computer.ComputerBuilder(
-				"500 GB", "2 GB").setBluetoothEnabled(true)
-				.setGraphicsCardEnabled(true).build();
-System.out.println("I am done");
-
+ import java.util.*;
+ interface Item {
+	public String name();
+	public float price();
 	}
+ class house
+{
+   private List<Item> items = new ArrayList<Item>();	
 
+   public void addItem(Item item){
+      items.add(item);
+   }
+
+public float getcost()
+{
+float cost =0.0f;
+for(Item item :items)
+{
+cost+=item.price();
 }
+return cost;
+}
+public void showitems()
+{for (Item item : items) {
+         System.out.print("Item : " + item.name());
+         System.out.println(", Price : " + item.price());
+      }		
+   }	
+}
+ class ownhouse implements Item {
+
+   @Override
+   public float price() {
+      return 500000.0f;
+   }
+
+   @Override
+   public String name() {
+      return "ownhouse";
+   }
+}
+
+
+
+ class renthouse implements Item {
+
+   @Override
+   public float price() {
+      return 5000.0f;
+   }
+
+   @Override
+   public String name() {
+      return "Rent house";
+   }
+}
+ class Builder {
+
+   public house prepareownhouse (){
+      house hb = new house();
+      
+      hb.addItem(new ownhouse ());
+      return hb;
+   }   
+
+   public house preparerenthouse (){
+      house hb = new house();
+      hb.addItem(new renthouse());
+      return hb;
+   }
+}
+
+
+public class TestBuilderPattern
+{
+public static void main(String[] s)
+{
+Builder hob=new Builder();
+  house rent = hob.preparerenthouse();
+System.out.println("Rent House");
+ rent.showitems();
+     
+  house own = hob.prepareownhouse();
+      System.out.println("\n\nOwn house");
+      own.showitems();
+      
+   }
+}
+
